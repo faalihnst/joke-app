d7aed2054807430c2719c6ab667796dd98b8c955
Faalih Nasution <faalihnst@gmail.com>
2025-02-20 00:33:40 +0700

    feat: initial commit, list of jokes, tested using android

--- App.tsx
+++ App.tsx
@@ -5,113 +5,283 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import axios from 'axios';
+import React, {memo, useEffect, useState} from 'react';
+import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  Button,
+  Card,
+  Dialog,
+  Divider,
+  Headline,
+  List,
+  PaperProvider,
+  Portal,
+} from 'react-native-paper';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const RightAccordionSection: React.FC<{
+  expanded: boolean;
+  handleGoTop: any;
+  setExpanded: Function;
+}> = ({expanded = false, handleGoTop = () => {}, setExpanded = () => {}}) => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.flexRow}>
+      <Button
+        buttonColor="green"
+        textColor="white"
+        style={{marginRight: 5}}
+        onPress={() => {
+          setExpanded(!expanded);
+          handleGoTop();
+        }}>
+        Go Top
+      </Button>
+      <List.Icon icon={expanded ? 'chevron-up' : 'chevron-down'} />
     </View>
   );
+};
+
+const Section: React.FC<{
+  category: Categories;
+  index: number;
+  handleGoTop: any;
+  showDialog: any;
+  fetchJokesData: Function;
+}> = memo(
+  ({
+    category = {},
+    index = 0,
+    handleGoTop = () => {},
+    showDialog = () => {},
+    fetchJokesData = () => {},
+  }) => {
+    const [expanded, setExpanded] = useState(false);
+    useEffect(() => {
+      fetchJokesData(category.category);
+    }, []); // eslint-disable-line
+
+    const _handleExpand = () => {
+      setExpanded(!expanded);
+    };
+
+    return (
+      <View style={styles.container}>
+        <Card style={styles.card}>
+          <List.Accordion
+            pointerEvents="box-none"
+            title={category?.category}
+            expanded={expanded}
+            onPress={_handleExpand}
+            right={props => (
+              <RightAccordionSection
+                {...props}
+                expanded={expanded}
+                handleGoTop={handleGoTop}
+                setExpanded={setExpanded}
+              />
+            )}
+            left={props => (
+              <List.Item
+                {...props}
+                titleStyle={{fontWeight: 'bold'}}
+                title={index + 1}
+              />
+            )}>
+            <>
+              <Text style={styles.jokeListHeader}>
+                {' '}
+                List of jokes based on {category.category} categories
+              </Text>
+              {category.jokes &&
+                category.jokes.map((item: any, idx: number) => (
+                  <TouchableOpacity onPress={() => showDialog(item?.joke)}>
+                    <View style={styles.jokesContainer}>
+                      <Text style={styles.indexStyle}>{idx + 1}</Text>
+                      <List.Item
+                        key={idx}
+                        title={item.joke}
+                        titleNumberOfLines={10}
+                      />
+                    </View>
+                    <Divider />
+                  </TouchableOpacity>
+                ))}
+              {category?.jokes?.length < 6 && (
+                <Button
+                  compact
+                  mode="text"
+                  onPress={() => fetchJokesData(category.category)}>
+                  Add More
+                </Button>
+              )}
+            </>
+          </List.Accordion>
+        </Card>
+      </View>
+    );
+  },
+);
+
+const JokeDialog: React.FC<{
+  joke: string;
+  visible: boolean;
+  setVisible: any;
+}> = memo(({joke = '', visible = false, setVisible = () => {}}) => {
+  return (
+    <Portal>
+      <Dialog visible={visible} onDismiss={() => setVisible(false)}>
+        <Dialog.Title>Random Joke</Dialog.Title>
+        <Dialog.Content>
+          <Text>{joke}</Text>
+        </Dialog.Content>
+        <Dialog.Actions>
+          <Button onPress={() => setVisible(false)}>Close</Button>
+        </Dialog.Actions>
+      </Dialog>
+    </Portal>
+  );
+});
+
+interface Categories {
+  category: string;
+  index: number;
+  jokes: Array<string>;
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App: React.FC = () => {
+  const [categories, setCategories] = useState<Categories[]>([]);
+  const [showDialog, setShowDialog] = useState(false);
+  const [jokeOnDialog, setJokeOnDialog] = useState('');
+  const [refreshing, setRefreshing] = useState(false);
+  useEffect(() => {
+    _fetchCategories();
+  }, []);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const _fetchCategories = () => {
+    axios.get('https://v2.jokeapi.dev/categories').then(res => {
+      if (res.status === 200) {
+        const categoriesTemp = res.data.categories;
+        const categoriesList = categoriesTemp.map(
+          (category: string, index: number) => ({
+            category,
+            index,
+            jokes: [],
+          }),
+        );
+        setCategories(categoriesList);
+      }
+    });
+  };
+
+  const _fetchJokes = (categoryName: string) => {
+    axios
+      .get(`https://v2.jokeapi.dev/joke/${categoryName}?type=single&amount=2`)
+      .then(res => {
+        if (res.status === 200) {
+          const jokesTemp = res.data.jokes;
+          setCategories(prevCategories =>
+            prevCategories.map(category =>
+              category.category === categoryName
+                ? {...category, jokes: [...category.jokes, ...jokesTemp]}
+                : category,
+            ),
+          );
+        }
+      });
+  };
+
+  const _moveToFront = (index: number) => {
+    setCategories(prevCategories => {
+      if (index > -1) {
+        prevCategories.unshift(prevCategories.splice(index, 1)[0]);
+      }
+      return [...prevCategories];
+    });
+  };
+
+  const _handleShowDialog = (joke: string) => {
+    setShowDialog(true);
+    setJokeOnDialog(joke);
+  };
+
+  const _onRefresh = () => {
+    setCategories([]);
+    setRefreshing(true);
+    _fetchCategories();
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 1000);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <PaperProvider>
+      <Headline style={{textAlign: 'center', fontWeight: 'bold'}}>
+        Joke App
+      </Headline>
+      <FlatList
+        refreshing={refreshing}
+        onRefresh={_onRefresh}
+        style={{marginBottom: 20}}
+        data={categories}
+        keyExtractor={item => item.category}
+        initialNumToRender={5}
+        renderItem={({item, index}) => (
+          <Section
+            category={item}
+            index={index}
+            handleGoTop={() => _moveToFront(index)}
+            showDialog={_handleShowDialog}
+            fetchJokesData={_fetchJokes}
+          />
+        )}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <JokeDialog
+        visible={showDialog}
+        setVisible={setShowDialog}
+        joke={jokeOnDialog}
+      />
+    </PaperProvider>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  flexRow: {
+    flexDirection: 'row',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#f0f0f0',
   },
-  sectionDescription: {
-    marginTop: 8,
+  card: {
+    width: '95%',
+    borderRadius: 12,
+    elevation: 5,
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    flex: 1,
+    borderColor: '#000',
+  },
+  jokeListHeader: {
+    marginBottom: 20,
+    textAlign: 'center',
+    marginTop: 5,
+  },
+  jokesContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 18,
+  },
+  indexStyle: {
+    fontWeight: 'bold',
     fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    color: 'black',
   },
 });
 
